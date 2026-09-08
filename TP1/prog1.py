12e3bb76a48e424e0a1033dc84425c25f79ff257
Morcire <aaa@bbb.fr>
2026-09-08 17:13:15 +0200

    ajout fonction puissance et tests

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+import fonctions as f
+
+while True:
+    a = int(input("Saisir le premier nombre : "))
+    b = int(input("Saisir le deuxième nombre : "))
+
+    res = f.puissance(a, b)
+    print("Résultat :", res)
